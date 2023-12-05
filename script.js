ba5ef3d3d3848358755bcec0f436fa3750c3f220
SirIyanu <105286160+SirIyanu@users.noreply.github.com>
2023-12-05 09:35:42 +0100

    second assignment

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
-function checkForEvenNum(number ){
-    return number % 2;
-};
+// function checkForEvenNum(number ){
+//     return number % 2;
+// };
 
-  let number = 8
+//   let number = 8
 
-  let remainder =(number % 2)
+//   let remainder =(number % 2)
 
-  if (remainder == 0){
-    console.log(`${number} is even`)
-  }else{
-    console.log(`${number} is odd`)
+//   if (remainder == 0){
+//     console.log(`${number} is even`)
+//   }else{
+//     console.log(`${number} is odd`)
+//   }
+
+
+// console.log (checkForEvenNum(
+// "8"
+// )
+// )
+
+
+ function printToConsole(n1, n2){
+      const parameters={
+          number1: n1,
+          number2: n2,
+      }
+  
+      return parameters
+       
+  
   }
-
-
-console.log (checkForEvenNum(
-"8"
-)
-)
+  console.log(printToConsole(
+      1, 2
+  ))
+  
+  console.log(printToConsole(
+      6, 7
+  ))
 
 
 
