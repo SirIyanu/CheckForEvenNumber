78f2bfe94443f727ffd7e94674506f8a53590f6d
SirIyanu <105286160+SirIyanu@users.noreply.github.com>
2023-12-05 09:04:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function checkForEvenNum(number ){
+    return number % 2;
+};
+
+  let number = 8
+
+  let remainder =(number % 2)
+
+  if (remainder == 0){
+    console.log(`${number} is even`)
+  }else{
+    console.log(`${number} is odd`)
+  }
+
+
+console.log (checkForEvenNum(
+"8"
+)
+)
+
+
+
